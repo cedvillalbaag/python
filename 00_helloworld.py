c47d773231ec003cb03a515cb9f60a33e6391379
Carlos Villalba <71088728+cedvillalbaag@users.noreply.github.com>
2021-05-14 15:27:57 -0500

    Update 00_helloworld.py

--- 00_helloworld.py
+++ 00_helloworld.py
@@ -1,3 +1,19 @@
+#SINTAXIS BASICA
+
 #Comentarios generales
 #este es otro
+
+
+#Comando para imprimir
 print ("Hello world")
+
+
+#Utilizar variables
+alfa = 0.5
+beta = 'Carlos'
+
+
+#Identacion
+#Utilizada en funciones, bucles o condicionales
+
+
