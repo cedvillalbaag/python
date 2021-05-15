95e4064a0c9061463d2e01525dfb257dbd6ffc4b
Carlos Villalba <cedvillalbaag@gmail.com>
2021-05-15 14:12:37 -0500

    actualizar

--- 10_loopswhile.py
+++ 10_loopswhile.py
@@ -1,3 +1,6 @@
+#Importar librerías
+import math
+
 ##########################################################################
 #BUCLE WHILE (SIMILAR A IF) - "CUANDO"
 
@@ -21,3 +24,33 @@ while var1 < 20:
     print ("Ejecución de código: " + str(var1))
     var1 = var1 + 1
 print("Termino la ejecución")
+
+
+
+###############################################################################
+#OTRO EJEMPLO
+
+edad = int(input("Ingrese su edad: "))
+
+###############################################################################
+#SALIR DEL BUCLE CON EL USO DE BREAK
+
+print("calculo de raiz cuadrada")
+
+
+numero = int(input("Agregue un numero:  "))
+
+intentos = 0
+
+while numero < 0:
+    print("No se puede hallar la raiz de numero negativo")
+
+    if intentos == 2:
+        print("Haz consumido demasiados intentos")
+        break;
+    numero = int(input("Agregue numero por favor:  "))
+    if numero < 0:
+        intentos = intentos + 1
+if intentos < 2:
+    solucion = math.sqrt(numero)
+    print (int(solucion))
