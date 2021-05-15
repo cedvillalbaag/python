41e15cc66c8b32b0e660018e311c435a8f0e29d3
Carlos Villalba <cedvillalbaag@gmail.com>
2021-05-15 12:06:04 -0500

    actualizar

--- 10_loopswhile.py
+++ 10_loopswhile.py
@@ -1,13 +1,23 @@
 ##########################################################################
-#BUCLE WHILE (SIMILAR A IF) - BUCLE INDETERMINADO
+#BUCLE WHILE (SIMILAR A IF) - "CUANDO"
 
-#Bucle indeterminado
+#Bucle indeterminado: no sabemos cuantas veces se irá a ejecutar
+
 #Sintaxis
-
 count = 4
 
-while count <= 10:
+while count < 10:
     print(count)
     count = count + 1
 
 
+###########################################################################
+#Otro ejemplo - Concatenando con texto
+#Importante validar las condiciones y el incremento para que finalice el bucle
+
+var1 = 5
+
+while var1 < 20:
+    print ("Ejecución de código: " + str(var1))
+    var1 = var1 + 1
+print("Termino la ejecución")
