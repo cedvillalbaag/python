abfab55f2f5ef4e9a68fb56dde7fafb3b4c52ee4
Carlos Villalba <cedvillalbaag@gmail.com>
2021-05-15 19:27:18 -0500

    actualizar

---
+++ 15_modulos.py
@@ -0,0 +1,21 @@
+#MODULOS
+
+#Archivo .py que posee sus propias variables, funciones, incluso otros módulos.
+#Sirven para organizar y reutilizar código que se ha escrito anteriormente.
+
+
+#########################################################
+
+#PASOS:
+
+#1.- Desarrollar código para modulo y guardar como archivo .py
+
+#2.- importar modulo al archivo
+# Modulo completo: import nombremodulo , en el archivo donde va a ser utilizado
+                # from nombremodulo import *
+
+# importar funciones del modulo: from nombremodulo import nombrefuncion
+
+#3.- para utilizar las funciones colocar primero el nombre del modulo y luego la funcion. Ejp: nombremodulo.funcion()
+
+#4.- A través de los paquetes se pueden reutilizar los módulos independientemente de donde esten ubicados.
