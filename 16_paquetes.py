e12f63889e2c188760b89e6385ef826e20fb0e8e
Carlos Villalba <cedvillalbaag@gmail.com>
2021-05-15 20:10:02 -0500

    actualizar

---
+++ 16_paquetes.py
@@ -0,0 +1,17 @@
+#PAQUETES
+
+#Carpeta donde se almacenarán módulos relacionados entre sí.
+
+#Sirven organizar y reutilizar módulos
+
+#########################################################################
+
+#COMO SE CREA UN PAQUETE
+
+#1.- Crear una carpeta con un archivo .py que tenga de nombre: __init__.py
+
+#2.- Crear o copiar los modulos en la carpeta
+
+#3.- Importar desde archivo de desarrollo mediante: from nombrecarpeta.nombremodulo import nombrefuncion
+
+#4.- Se pueden crear subpaquetes. Recordar que se debe segui la ruta cuando se importe , y en cada carpeta craer el archivo __init__.py
