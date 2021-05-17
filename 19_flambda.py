4489abf16f870676001cab0d5d68bf683f9dbdf9
Carlos Villalba <cedvillalbaag@gmail.com>
2021-05-17 13:07:02 -0500

    actualizar

---
+++ 19_flambda.py
@@ -0,0 +1,13 @@
+#FUNCIONES LAMBDA 
+
+#se refiere a generar funciones sencillas
+
+
+#######################################
+
+#EJEMPLO
+
+area_triangulo = lambda base,altura: (base*altura)/2
+
+
+print(area_triangulo(1,4))
